9dd2f6b48c4a0ade0080750e075803357b02740e
Matej398 <matej.resman@gmail.com>
2025-12-23 21:43:24 +0100

    Use relative base for v1/v2 and relative landing links

--- versions/v1-src/vite.config.v1.mjs
+++ versions/v1-src/vite.config.v1.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/v1/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: '../../dist/v1',
